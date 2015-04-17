6819774dea2c4ebf0a02afda4e6416e47f3ba372
Yaniv Savir <saviry@gmail.com>
2015-04-17 15:53:21 -0400

    preps testing configs for using global settings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,7 @@ require 'spork'
 
 Spork.prefork do
   RSpec.configure do |config|
+
     config.expect_with :rspec do |expectations|
       expectations.include_chain_clauses_in_custom_matcher_descriptions = true
     end
